2efa6c80caa7b57f99b4944a27b4011b1d68d80e
parixscm <parixscm@gmail.com>
2023-03-03 02:15:08 +0900

    ✨ Feat: ADD nextauth authentication functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,33 @@
+import { SessionProvider } from "../components/SessionProvider";
+import { getServerSession } from "next-auth";
 import SideBar from "../components/SideBar";
 import "../styles/globals.css";
+import { getServers } from "dns";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
       <head />
       <body>
-        <div className="flex">
-          <div className="max-w-xs h-screen overflow-y-auto bg-[#202123] md:min-w-[20rem]">
-            <SideBar />
+        <SessionProvider session={session}>
+          <div className="flex">
+            <div className="max-w-xs h-screen overflow-y-auto bg-[#202123] md:min-w-[20rem]">
+              <SideBar />
+            </div>
+
+            {/* Client Provider - Notification */}
+
+            {/* Content */}
+            <div className="bg-[#343541] flex-1">{children}</div>
           </div>
-
-          {/* Client Provider - Notification */}
-
-          {/* Content */}
-          <div className="bg-[#343541] flex-1">{children}</div>
-        </div>
+        </SessionProvider>
       </body>
     </html>
   );
