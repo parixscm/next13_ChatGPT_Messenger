737d346e6430ec04cb2fbb72cf0c1c5c5102dfeb
parixscm <parixscm@gmail.com>
2023-03-02 18:00:11 +0900

    📝 Feat: ADD newchat component to sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,10 +1,11 @@
+import NewChat from "./NewChat";
+
 function SideBar() {
   return (
     <div className="p-2 h-screen flex flex-col">
       <div className="flex-1">
         <div>
-          {/* New Chat */}
-          SideBar Goes Here
+          <NewChat />
           <div>{/* Model Selection */}</div>
           {/* Map Through the Chat Rows */}
         </div>
